85a314b72970d54ee2a7900df5f8a6af26219698
Shayan Baloch <shayanbalochofficial@gmail.com>
2026-07-27 04:02:50 +0500

    Creating Meme Grids

---
+++ MemeWebsite/script.js
@@ -0,0 +1,11 @@
+const gallery = document.getElementById("gallery");
+
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
